8bbba55746ec8de9a7beaec9304a6ae279865713
deniz <oezdemir.de@gmail.com>
2024-02-12 12:03:13 +0100

    spo on cleanup

--- draw_map.c
+++ draw_map.c
@@ -6,21 +6,18 @@
 /*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 14:43:43 by denizozd          #+#    #+#             */
-/*   Updated: 2024/02/12 11:46:22 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/02/12 12:02:25 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static float	get_abs(float x)
+static float	get_max_abs(float x, float y)
 {
 	if (x < 0)
 		x = -x;
-	return (x);
-}
-
-static float	get_max(float x, float y)
-{
+	if (y < 0)
+		y = -y;
 	if (x >= y)
 		return (x);
 	else
@@ -39,7 +36,7 @@ void	custom_mlx_pixel_put(t_fdf *data, int x1, int y1, int clr)
 	}
 }
 
-/*	bresenham line algo	*/
+/* Bresenham's line algorithm */
 void	draw_line(t_fdf *data)
 {
 	float	x_incr;
@@ -48,7 +45,7 @@ void	draw_line(t_fdf *data)
 	int		z1;
 	int		z2;
 
-	z1 = data->z_mtx[(int)data->y1][(int)data->x1] * data->scale;	// access value in matrix by row then column
+	z1 = data->z_mtx[(int)data->y1][(int)data->x1] * data->scale;
 	z2 = data->z_mtx[(int)data->y2][(int)data->x2] * data->scale;
 	zoom(data);
 	clr(data, z1, z2);
@@ -56,7 +53,7 @@ void	draw_line(t_fdf *data)
 	translate(data);
 	x_incr = data->x2 - data->x1;
 	y_incr = data->y2 - data->y1;
-	max_abs_incr = get_max(get_abs(x_incr), get_abs(y_incr));
+	max_abs_incr = get_max_abs(x_incr, y_incr);
 	x_incr = x_incr / max_abs_incr;
 	y_incr = y_incr / max_abs_incr;
 	while ((int)(data->y1 - data->y2) || (int)(data->x1 - data->x2))
@@ -83,6 +80,7 @@ void	set_coords(t_fdf *data, int i, int j, int flg)
 	}
 }
 
+/* For every point: first draw line to the right, then draw line downwards */
 void	draw_map(t_fdf *data)
 {
 	int	i;
@@ -97,12 +95,12 @@ void	draw_map(t_fdf *data)
 			if (i < data->width - 1)
 			{
 				set_coords(data, i, j, 0);
-				draw_line(data); // draw line to the right
+				draw_line(data);
 			}
 			if (j < data->height - 1)
 			{
 				set_coords(data, i, j, 1);
-				draw_line(data); // draw line downwards
+				draw_line(data);
 			}
 			i++;
 		}
